914c89da7b271d2ce85fb77224e56ca6d48ef086
Maxim Belousov <max.belousov.ne@gmail.com>
2022-08-16 14:46:48 +0300

    db connection mooved to App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data.Entity;
+using System.Windows;
+using EntityFraemworkPetProject.Model;
 
 namespace EntityFraemworkPetProject
 {
@@ -7,5 +10,25 @@ namespace EntityFraemworkPetProject
     /// </summary>
     public partial class App : Application
     {
+        internal MSSQLOnlineShopdbEntities DbContext { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DbContext = new MSSQLOnlineShopdbEntities();
+            try
+            {
+                DbContext.Customer.Load();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не удалось загрузить базу данных. Ошибка: {ex.Message}",
+                    "Ошибка загрузки базы данных", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            DbContext.Dispose();
+        }
     }
 }
